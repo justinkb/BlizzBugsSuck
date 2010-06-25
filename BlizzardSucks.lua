91ecbb8bf038f10d6d7e45f7d46f3d7c8c9b2052
Shefki <shefki@shefki.org>
2010-06-25 00:04:29 -0700

    Initial Commit, includes workaround for menus behind the background bug.

---
+++ BlizzardSucks.lua
@@ -0,0 +1,20 @@
+-- UIDropDownMenu FrameLevels do not properly follow their parent and need to be
+-- fixed to prevent the button being under the background.
+local function FixMenuFrameLevels()
+	for l=1,UIDROPDOWNMENU_MAXLEVELS do
+		for b=1,UIDROPDOWNMENU_MAXBUTTONS do
+			local button = _G["DropDownList"..l.."Button"..b]
+			if button then 
+				local button_parent = button:GetParent()
+				if button_parent then 
+					local button_level = button:GetFrameLevel()
+					local parent_level = button_parent:GetFrameLevel()
+					if button_level <= parent_level then 
+						button:SetFrameLevel(parent_level + 2) 
+					end  
+				end  
+			end  
+		end  
+	end  
+end
+hooksecurefunc("UIDropDownMenu_CreateFrames", FixMenuFrameLevels)
