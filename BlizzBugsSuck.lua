876e94be97d83a43f4475f16fa779802a4788f79
Phanx <addons@phanx.net>
2014-12-13 06:10:19 -0800

    Fix bad ITEM_QUALITY2_DESC translation in German the long way (fixes #18)

--- BlizzBugsSuck.lua
+++ BlizzBugsSuck.lua
@@ -4,17 +4,48 @@ wow_build = tonumber(wow_build)
 -- Fix incorrect translations in the German localization
 if GetLocale() == "deDE" then
 	-- Day one-letter abbreviation is using a whole word instead of one letter.
-	-- Confirmed still bugged in 6.0.2 build 19034
+	-- Confirmed still bugged in 6.0.3.19243
 	DAY_ONELETTER_ABBR = "%d d"
+
 	-- Quality 2 (Uncommon) is incorrectly using the same translation as Quality 3 (Rare).
 	-- Previously 2/3 were Selten/Rar; it looks like they meant to update these to match the
 	-- equivalent battle pet quality strings (Ungewöhnlich/Selten) but forgot to finish the job.
-	-- New bug in Warlords of Draenor
-	ITEM_QUALITY2_DESC = BATTLE_PET_BREED_QUALITY3
+	-- Confirmed still bugged in 6.0.3.19243
+	-- A simple global override breaks unit frame dropdowns -_-
+	-- ITEM_QUALITY2_DESC = BATTLE_PET_BREED_QUALITY3
+	-- ...so we have to do all this nonsense instead:
+	-- Fix it in the sub-menu
+	UnitPopupButtons["ITEM_QUALITY2_DESC"].text = BATTLE_PET_BREED_QUALITY3
+	-- Fix it in the top-level menu
+	hooksecurefunc("UnitPopup_ShowMenu", function(menu, which, unit)
+		local lootThreshold = GetLootThreshold()
+		if UIDROPDOWNMENU_MENU_VALUE == "LOOT_THRESHOLD" then
+			for index = 1, 2 do
+				local match = (index + 1) == lootThreshold
+				local buttonName = "DropDownList"..UIDROPDOWNMENU_MENU_LEVEL.."Button"..index
+				_G[buttonName.."Check"]:SetShown(match)
+				_G[buttonName.."UnCheck"]:SetShown(not match)
+			end
+		elseif lootThreshold == 2 and which == "SELF" and UIDROPDOWNMENU_MENU_VALUE == nil then
+			for index = 1, 20 do -- arbitrary upper bound
+				local button = _G["DropDownList"..UIDROPDOWNMENU_MENU_LEVEL.."Button"..index]
+				if button and button.value == "LOOT_THRESHOLD" then
+					return button:SetText(ITEM_QUALITY_COLORS[2].hex .. BATTLE_PET_BREED_QUALITY3)
+				end
+			end
+		end
+	end)
+	-- Fix it in chat frame system messages
+	local pattern = gsub(ERR_SET_LOOT_THRESHOLD_S, "%%s", ".+")
+	ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", function(_, _, message, ...)
+		if GetLootThreshold() == 2 and strmatch(message, pattern) then
+			return false, format(ERR_SET_LOOT_THRESHOLD_S, BATTLE_PET_BREED_QUALITY3), ...
+		end
+	end)
 end
 
 -- Fix InterfaceOptionsFrame_OpenToCategory not actually opening the category (and not even scrolling to it)
--- Confirmed still broken in 6.0.2 build 19034
+-- Confirmed still broken in 6.0.3.19243
 do
 	local function get_panel_name(panel)
 		local tp = type(panel)
@@ -71,8 +102,8 @@ do
 		end
 		local Smin, Smax = InterfaceOptionsFrameAddOnsListScrollBar:GetMinMaxValues()
 		if shownpanels > 15 and Smin < Smax then
-		  local val = (Smax/(shownpanels-15))*(mypanel-2)
-		  InterfaceOptionsFrameAddOnsListScrollBar:SetValue(val)
+			local val = (Smax/(shownpanels-15))*(mypanel-2)
+			InterfaceOptionsFrameAddOnsListScrollBar:SetValue(val)
 		end
 		doNotRun = true
 		InterfaceOptionsFrame_OpenToCategory(panel)
@@ -88,17 +119,19 @@ end
 
 -- Fix an issue where the PetJournal drag buttons cannot be clicked to link a pet into chat
 -- The necessary code is already present, but the buttons are not registered for the correct click
--- Confirmed still broken in 6.0.2 build 19034
-if true then
-        local frame = CreateFrame("Frame")
-        frame:RegisterEvent("ADDON_LOADED")
-        frame:SetScript("OnEvent", function(self, event, name)
-                if event == "ADDON_LOADED" and name == "Blizzard_PetJournal" then
-			for i=1,3 do
-				local d = _G["PetJournalLoadoutPet"..i]
-				d = d and d.dragButton
-				if d then d:RegisterForClicks("LeftButtonUp") end
+-- Confirmed still bugged in 6.0.3.19243
+do
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("ADDON_LOADED")
+	frame:SetScript("OnEvent", function(self, event, name)
+		if name == "Blizzard_PetJournal" then
+			for i = 1, 3 do
+				local button = _G["PetJournalLoadoutPet"..i]
+				if button and button.dragButton then
+					button.dragButton:RegisterForClicks("LeftButtonUp")
+				end
 			end
-                end
-        end)
+			self:UnregisterAllEvents()
+		end
+	end)
 end
