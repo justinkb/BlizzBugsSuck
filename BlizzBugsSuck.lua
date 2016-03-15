d959a5bd492f9a24d22fbdafb5c1a7cd1dec7f33
oscarucb <oscarucb@gmail.com>
2016-03-14 21:41:40 -0400

    Workaround for addon enable/disable glitch
    Fix the addon list sometimes incorrectly toggling the enable state of addons
    for other characters. The problem affects both Blizzard addon lists (in-game
    and login screen), as well as third-party addon control addons. This generic
    workaround addresses the problem for all these cases.

--- BlizzBugsSuck.lua
+++ BlizzBugsSuck.lua
@@ -146,3 +146,28 @@ do
 	end
 end
 
+
+-- Fix glitchy-ness of EnableAddOn/DisableAddOn API, which affects the stability of the default 
+-- UI's addon management list (both in-game and glue), as well as any addon-management addons.
+-- The problem is caused by broken defaulting logic used to merge AddOns.txt settings across 
+-- characters to those missing a setting in AddOns.txt, whereby toggling an addon for a single character 
+-- sometimes results in also toggling it for a different character on that realm for no obvious reason.
+-- The code below ensures each character gets an independent enable setting for each installed 
+-- addon in its AddOns.txt file, thereby avoiding the broken defaulting logic.
+-- Note the fix applies to each character the first time it loads there, and a given character 
+-- is not protected from the faulty logic on addon X until after the fix has run with addon X 
+-- installed (regardless of enable setting) and the character has logged out normally.
+-- Confirmed bugged in 6.2.3.20886
+do
+	local player = UnitName("player")
+	if player and #player > 0 then
+		for i=1,GetNumAddOns() do 
+			if GetAddOnEnableState(player, i) > 0 then  -- addon is enabled
+				EnableAddOn(i, player)
+			else
+				DisableAddOn(i, player)
+			end 
+		end
+	end
+end
+
